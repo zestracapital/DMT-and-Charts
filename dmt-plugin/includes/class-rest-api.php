3192ca4cb8ac63aa552617820efa989761f3d243
Malik Mehtab <malikmehtab857@gmail.com>
2025-09-09 15:02:52 +0500

    🔧 Fix: Create placeholder class-rest-api.php to prevent fatal errors

---
+++ dmt-plugin/includes/class-rest-api.php
@@ -0,0 +1,10 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class ZC_DMT_REST_API {
+    public function register_routes() {
+        // Placeholder - routes will be added later
+    }
+}
