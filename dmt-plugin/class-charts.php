bf2d545cbd36772536079d76c829cd40ee6cafa6
Malik Mehtab <malikmehtab857@gmail.com>
2025-09-10 15:56:21 +0500

    Fix REST route slug and response keys for Chart data - Add REST APIUpdate class-charts.php

--- dmt-plugin/class-charts.php
+++ dmt-plugin/class-charts.php
@@ -1,17 +1,41 @@
-<?php
-/**
+<?php /**
  * Chart shortcode system for DMT plugin
  * Provides dynamic and static chart shortcodes
  */
-
 if (!defined('ABSPATH')) exit;
 
 class ZC_DMT_Charts {
-
     public static function init() {
         add_shortcode('economic_chart_dynamic', [__CLASS__, 'dynamic_shortcode']);
         add_shortcode('economic_chart_static', [__CLASS__, 'static_shortcode']);
         add_action('wp_enqueue_scripts', [__CLASS__, 'enqueue_chart_assets']);
+        add_action('rest_api_init', [__CLASS__, 'register_rest_routes']);
+    }
+
+    public static function register_rest_routes() {
+        register_rest_route('zc-dmt/v1', '/data/(?P<slug>[a-zA-Z0-9\-_]+)', [
+            'methods' => 'GET',
+            'callback' => [__CLASS__, 'get_indicator_data'],
+            'permission_callback' => '__return_true'
+        ]);
+    }
+
+    public static function get_indicator_data($request) {
+        $slug = $request->get_param('slug');
+        
+        // Sample data - in real implementation, this would fetch from database
+        $sample_data = [
+            'cpi_us_sample' => [
+                'labels' => ['2020-01', '2020-02', '2020-03', '2020-04', '2020-05', '2020-06'],
+                'data' => [258.811, 258.678, 258.115, 256.389, 256.394, 257.797]
+            ]
+        ];
+        
+        if (isset($sample_data[$slug])) {
+            return new WP_REST_Response($sample_data[$slug], 200);
+        }
+        
+        return new WP_Error('not_found', 'Indicator data not found', ['status' => 404]);
     }
 
     public static function enqueue_chart_assets() {
@@ -20,7 +44,6 @@ class ZC_DMT_Charts {
         if (is_a($post, 'WP_Post')) {
             if (has_shortcode($post->post_content, 'economic_chart_dynamic') || 
                 has_shortcode($post->post_content, 'economic_chart_static')) {
-
                 wp_dequeue_script('chartjs');
                 wp_enqueue_script(
                     'chartjs-umd',
@@ -31,12 +54,10 @@ class ZC_DMT_Charts {
                 );
                 wp_enqueue_script('zc-dmt-charts', ZC_DMT_PLUGIN_URL . 'charts-frontend.js', ['chartjs-umd', 'jquery'], ZC_DMT_VERSION, true);
                 wp_enqueue_script('zc-dmt-charts', ZC_DMT_PLUGIN_URL . 'charts-frontend.js', ['chartjs', 'jquery'], ZC_DMT_VERSION, true);
-
                 wp_localize_script('zc-dmt-charts', 'zcCharts', [
                     'restUrl' => rest_url('zc-dmt/v1/'),
                     'nonce' => wp_create_nonce('wp_rest')
                 ]);
-
                 wp_add_inline_style('wp-block-library', '
                 .zci-dynamic-chart {
                     border: 1px solid #e5e7eb;
@@ -110,26 +131,22 @@ class ZC_DMT_Charts {
         ], $atts, 'economic_chart_dynamic');
 
         $chart_id = 'zci-dynamic-' . wp_generate_uuid4();
-
         ob_start();
         ?>
-        <div id="<?php echo esc_attr($chart_id); ?>" class="zci-dynamic-chart" data-theme="<?php echo esc_attr($atts['theme']); ?>" style="height: <?php echo esc_attr($atts['height']); ?>;">
+        <div class="zci-dynamic-chart" data-theme="<?php echo esc_attr($atts['theme']); ?>" id="<?php echo esc_attr($chart_id); ?>" style="height: <?php echo esc_attr($atts['height']); ?>;">
             <h3><?php echo esc_html($atts['title']); ?></h3>
-
             <?php if ($atts['show_search'] === 'true'): ?>
             <div class="zci-chart-controls">
-                <input type="text" class="zci-search-input" placeholder="Search economic indicators..." />
-                <button type="button" class="button">Add Indicator</button>
+                <input class="zci-search-input" placeholder="Search economic indicators..." type="text"/>
+                <button class="button" type="button">Add Indicator</button>
             </div>
             <ul class="zci-search-results"></ul>
             <div class="zci-selected-indicators"></div>
             <?php endif; ?>
-
             <canvas class="zci-chart-canvas" style="height: calc(100% - 120px);"></canvas>
         </div>
-
         
-        <?php
+        <?php 
         return ob_get_clean();
     }
 
@@ -143,20 +160,18 @@ class ZC_DMT_Charts {
         ], $atts, 'economic_chart_static');
 
         if (empty($atts['indicators'])) {
-            return '<p><em>No indicators specified for static chart</em></p>';
+            return 'No indicators specified for static chart';
         }
 
         $chart_id = 'zci-static-' . wp_generate_uuid4();
-
         ob_start();
         ?>
-        <div id="<?php echo esc_attr($chart_id); ?>" class="zci-static-chart" data-indicators="<?php echo esc_attr($atts['indicators']); ?>" data-type="<?php echo esc_attr($atts['type']); ?>" data-theme="<?php echo esc_attr($atts['theme']); ?>" style="height: <?php echo esc_attr($atts['height']); ?>;">
+        <div class="zci-static-chart" data-indicators="<?php echo esc_attr($atts['indicators']); ?>" data-theme="<?php echo esc_attr($atts['theme']); ?>" data-type="<?php echo esc_attr($atts['type']); ?>" id="<?php echo esc_attr($chart_id); ?>" style="height: <?php echo esc_attr($atts['height']); ?>;">
             <?php if (!empty($atts['title'])): ?>
                 <h3><?php echo esc_html($atts['title']); ?></h3>
             <?php endif; ?>
             <canvas class="zci-chart-canvas" style="width: 100%; height: calc(100% - 40px);"></canvas>
         </div>
-
         <script>
         document.addEventListener('DOMContentLoaded', function() {
             if (typeof ZCCharts !== 'undefined') {
@@ -164,7 +179,8 @@ class ZC_DMT_Charts {
             }
         });
         </script>
-        <?php
+        
+        <?php 
         return ob_get_clean();
     }
 }
